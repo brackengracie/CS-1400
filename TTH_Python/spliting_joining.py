389506a48af97032b1215ec6da5d1d225953fa7a
Gracie Bracken <brackengracie@gmail.com>
2018-04-02 08:50:11 -0600

    Team Tree House

---
+++ TTH_Python/spliting_joining.py
@@ -0,0 +1,7 @@
+flavors = ["vanilla", "strawberry"]
+", ".join(flavors)
+return flavors
+
+"My favorite flavors favorite:" + ", ".join(flavors)
+#only can use join for strings
+"my favorite flavors are: {}"".format(", ".join(flavors))
