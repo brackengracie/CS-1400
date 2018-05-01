85773d39f20b9a239c7191a2f2afe528822336f9
Gracie Bracken <brackengracie@gmail.com>
2018-05-01 08:30:50 -0600

    cleaning up

--- TTH_Python/spliting_joining.py
+++ TTH_Python/spliting_joining.py
@@ -5,3 +5,9 @@ return flavors
 "My favorite flavors favorite:" + ", ".join(flavors)
 #only can use join for strings
 "my favorite flavors are: {}"".format(", ".join(flavors))
+
+
+available = "Banana split;hot fudge;cherry;malted;black and white"
+sundaes = available.split(";")
+display_menu = ",".join(sundaes)
+menu = "our available flavors are: {}.".format(display_menu)
